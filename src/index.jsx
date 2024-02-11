6f6358072a11b9a1fda12e52b87376092539776e
Michael SDC <michael.sundelacruz@gmail.com>
2024-02-11 13:30:34 -0500

    lesson 60 complete

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,16 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App.jsx";
 import "./index.css";
 
 const entryPoint = document.getElementById("root");
-ReactDOM.createRoot(entryPoint).render(<App />);
+
+
+// ReactDOM.createRoot(entryPoint).render(<App />);
+
+// This code below is an example of how to avoid using the JSX syntax
+// and use the React.createElement function instead.
+// See the App used below is not a JSX tag, but a function call.
+
+ReactDOM.createRoot(entryPoint).render(React.createElement(App));
